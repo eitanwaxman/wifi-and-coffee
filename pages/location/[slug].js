594d2ba321c94c03116f7ddaaf92db986f6e6175
eitanwaxman <eitanwaxman@gmail.com>
2022-12-27 21:53:27 +0000

    review scroll

--- pages/location/[slug].js
+++ pages/location/[slug].js
@@ -243,6 +243,8 @@ export default function Location() {
                             left: '50%',
                             transform: 'translate(-50%, -50%)',
                             width: 400,
+                            maxHeight: 600,
+                            overflowY: "scroll",
                             bgcolor: 'background.paper',
                             boxShadow: 24,
                             p: 4,
